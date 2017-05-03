024157a6025ac607c18ee5d9d0a7974cfc6b9646
Elena Savinova <elena@unity3d.com>
2017-05-03 21:31:51 +0300

    clicker

---
+++ Assets/Scenes/Networking/Scripts/MiniMapClicker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MiniMapClicker : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject miniMap;
+    public Camera miniCamera;
+    public ISRTSCamera mainCamera;
+
+	void Start () 
+    {
+        miniCamera = miniMap.GetComponent<Camera>();
+
+        mainCamera = Camera.main.GetComponent<ISRTSCamera> ();
+		
+	}
+	
+    public void Update ()
+    {        
+        if (mainCamera.followingTarget == null)
+            return;
+                   
+        //Debug.Log (Input.GetMouseButtonDown(1));
+        if (Input.GetMouseButtonDown (1) && miniCamera.pixelRect.Contains (Input.mousePosition)) 
+        {
+            //Debug.Log (Input.mousePosition);
+            var point = miniCamera.ScreenToWorldPoint (Input.mousePosition);
+            var follow = mainCamera.followingTarget;
+            follow.position = point;
+            mainCamera.Follow (follow);
+        }
+    }
+
+    public void OnPointerClick (PointerEventData eventData)
+    {
+        //throw new NotImplementedException ();
+        Debug.Log ("I was clicked");
+    }
+}
