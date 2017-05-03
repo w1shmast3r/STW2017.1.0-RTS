cbc02b481146229a984daaf463245aac56ca3fee
DelWise <turshatov@gmail.com>
2017-05-03 18:38:23 +0300

    Clean UnitSpawner

--- Assets/Scripts/Spawner/UnitSpawner.cs
+++ Assets/Scripts/Spawner/UnitSpawner.cs
@@ -15,96 +15,10 @@ public class UnitSpawner : MonoBehaviour, IPointerClickHandler
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("In OnPointerClick");
-        if (units.Count < 1)        {
-            Debug.LogError("You need to add units before spawning.");
-            return;
-        }
-
         ShowBuildingMenu();
     }
 
-
-    public void SpawnUnit()
-    {
-        var unit = Instantiate(units[0], spawnPosition.position, Quaternion.identity) as GameObject;
-        NavMeshAgent agent = unit.GetComponent<NavMeshAgent>();
-        agent.SetDestination(meetPoint.position);
-
-       // var mover = unit.GetComponent<UnitMover>();
-       // NetworkServer.Spawn(unit);
-        //mover.SetDestination(meetPoint1.position);
-    }
-
-
-    public void SpawnUnit1()
-    {
-        var unit = Instantiate(units[1], spawnPosition.position, Quaternion.identity) as GameObject;
-        NavMeshAgent agent = unit.GetComponent<NavMeshAgent>();
-        agent.SetDestination(meetPoint.position);
-
-        // var mover = unit.GetComponent<UnitMover>();
-        // NetworkServer.Spawn(unit);
-        //mover.SetDestination(meetPoint1.position);
-    }
-
-    public void SpawnUnit2()
-    {
-        var unit = Instantiate(units[2], spawnPosition.position, Quaternion.identity) as GameObject;
-        NavMeshAgent agent = unit.GetComponent<NavMeshAgent>();
-        agent.SetDestination(meetPoint.position);
-
-        // var mover = unit.GetComponent<UnitMover>();
-        // NetworkServer.Spawn(unit);
-        //mover.SetDestination(meetPoint1.position);
-    }
-
-    public void SpawnUnit3()
-    {
-        var unit = Instantiate(units[3], spawnPosition.position, Quaternion.identity) as GameObject;
-        NavMeshAgent agent = unit.GetComponent<NavMeshAgent>();
-        agent.SetDestination(meetPoint.position);
-
-        // var mover = unit.GetComponent<UnitMover>();
-        // NetworkServer.Spawn(unit);
-        //mover.SetDestination(meetPoint1.position);
-    }
-
-    public void SpawnUnit4()
-    {
-        var unit = Instantiate(units[4], spawnPosition.position, Quaternion.identity) as GameObject;
-        NavMeshAgent agent = unit.GetComponent<NavMeshAgent>();
-        agent.SetDestination(meetPoint.position);
-
-        // var mover = unit.GetComponent<UnitMover>();
-        // NetworkServer.Spawn(unit);
-        //mover.SetDestination(meetPoint1.position);
-    }
-    //    [Command]
-    //    private void CmdSpawnUnit()
-    //    {
-    //        var unit = Instantiate(GetRandomUnit(), spawnPosition.position, Quaternion.identity) as GameObject;
-    //        var mover = unit.GetComponent<UnitMover>();
-    //        NetworkServer.Spawn(unit);
-    //        mover.SetDestination(meetPoint.position);
-    //    }
-
-    //    [Command]
-    //    public void CmdSpawnUnit(int index) {
-    //        var unit = Instantiate(units[index], spawnPosition.position, Quaternion.identity) as GameObject;
-    //        var mover = unit.GetComponent<UnitMover>();
-    //        NetworkServer.Spawn(unit);
-    //        mover.SetDestination(meetPoint.position);
-    //    }
-    //
     private void ShowBuildingMenu() {
-        //buildingMenu.SetActive(true);
         LevelController.buildingMenu.SetActive(true);
-
-    }
-
-    private GameObject GetRandomUnit()
-    {
-        return units[Random.Range(0, units.Count)];
     }
 }
