26bdaf50fc15a85f686303f182e3b76a17d50069
DelWise <turshatov@gmail.com>
2017-05-05 16:09:27 +0300

    Fix unit's spawn and meet position

--- Assets/Scripts/Spawner/UnitSpawner.cs
+++ Assets/Scripts/Spawner/UnitSpawner.cs
@@ -10,24 +10,20 @@ public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
     public Transform meetPoint;
     public List<GameObject> units;
     public GameObject buildingMenu;
-   // public NavMeshAgent agent;
-
-
+   
     public void Start()
     {
-        var netID = GetComponent<NetworkIdentity>().netId.Value;
-        if (TeamHandler_NW.teamHandler.GetMyTeam(netID) == LevelController.myTeam)
+        if (GetComponent<Base_NW>().team == LevelController.myTeam)
         {
             LevelController.spawnPoint = spawnPosition.position;
             LevelController.meetPoint = meetPoint.position;
         }
-
-        
     }
     
     public void OnPointerClick(PointerEventData eventData)
     {
-        ShowBuildingMenu();
+        if (GetComponent<Base_NW>().team == LevelController.myTeam)
+            ShowBuildingMenu();
     }
 
     private void ShowBuildingMenu() {
